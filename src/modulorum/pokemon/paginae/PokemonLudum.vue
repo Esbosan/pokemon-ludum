834e592db985194dcf4e1b5abff302ba2d598c8f
Esbosan <esthersanchobosch@gmail.com>
2025-03-12 17:32:11 +0100

    Pokemon al azar y preparada la visibilidad

--- src/modulorum/pokemon/paginae/PokemonLudum.vue
+++ src/modulorum/pokemon/paginae/PokemonLudum.vue
@@ -1,21 +1,26 @@
 <template>
-  <section v-if="false" class="flex flex-col justify-center items-center w-screen h-screen">
+  <section v-if="estPortat || temerePokemon.id === null" class="flex flex-col justify-center items-center w-screen h-screen">
     <h1 class="text-3xl">Espere, por favor</h1>
     <h3 class="animate-pulse">Cargando Pokémons</h3>
   </section>
 
-  <section class="flex flex-col justify-center items-center w-screen h-screen">
+  <section v-else class="flex flex-col justify-center items-center w-screen h-screen">
     <h1 class="m-5">¿Quién es este Pokémon?</h1>
+    <h3>{{ temerePokemon.nomen }}</h3>
 
-    <PokemonsImago />
+    <PokemonsImago :pokemon-id="temerePokemon.id"  :revelare_pokemon="LudumStatus !== LudumStatus.Ludit" />
     <PokemonOptiones />
-    
   </section>
 </template>
 
 <script setup lang="ts">
 import PokemonOptiones from '../components/PokemonOptiones.vue';
 import PokemonsImago from '../components/PokemonsImago.vue';
+import { usePokemonLudum } from '../composables/usePokemonLudu';
+
+const { LudumStatus, estPortat, temerePokemon } = usePokemonLudum();
+
+
 </script>
 
 <style scoped></style>
