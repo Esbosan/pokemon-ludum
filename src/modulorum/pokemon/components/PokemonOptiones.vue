0170090b92cebdc3d6146de656a82c4912b9badb
Esbosan <esthersanchobosch@gmail.com>
2025-03-14 18:27:32 +0100

    Pokemon con fallos

--- src/modulorum/pokemon/components/PokemonOptiones.vue
+++ src/modulorum/pokemon/components/PokemonOptiones.vue
@@ -1,22 +1,30 @@
 <template>
   <section class="flex flex-col mt-5">
     <button
-    v-for="{ nomen, id } in optiones"
-    @click="$emit('electusOptio', id)"
-    :key="id"
-    class="capitalize">
-
-
+      :disabled="true"
+      v-for="{ nomen, id } in optiones"
+      @click="$emit('electusOptio', id)"
+      :key="id"
+      :class="[
+        'capitalize disabled:shadow-none disables:bg-gray-100',
+        {
+          recte: id === recteResponsio && nonEligere,
+          'non-recte': id !== recteResponsio && nonEligere,
+        },
+      ]"
+    >
       {{ nomen }}
     </button>
   </section>
 </template>
 
 <script setup lang="ts">
-import type { Pokemon } from '../interfaces';
+import { LudumStatus, type Pokemon } from '../interfaces';
 
 interface Props {
   optiones: Pokemon[];
+  nonEligere: boolean;
+  recteResponsio: number;
 }
 
 defineProps<Props>();
@@ -31,5 +39,13 @@ defineEmits<{
 
 button {
   @apply bg-white shadow-md rounded-lg p-3 my-2 cursor-pointer w-40 text-center hover:bg-gray-100 transition-all;
+}
+
+.recte {
+  @apply bg-blue-500 text-white;
+}
+
+.non-recte {
+  @apply bg-red-100 opacity-70;
 }
 </style>
--- src/modulorum/pokemon/paginae/PokemonLudum.vue
+++ src/modulorum/pokemon/paginae/PokemonLudum.vue
@@ -1,5 +1,8 @@
 <template>
-  <section v-if="estPortat || temerePokemon.id === null" class="flex flex-col justify-center items-center w-screen h-screen">
+  <section
+    v-if="estPortat || temerePokemon.id === null"
+    class="flex flex-col justify-center items-center w-screen h-screen"
+  >
     <h1 class="text-3xl">Espere, por favor</h1>
     <h3 class="animate-pulse">Cargando Pokémons</h3>
   </section>
@@ -7,10 +10,27 @@
   <section v-else class="flex flex-col justify-center items-center w-screen h-screen">
     <h1 class="m-5">¿Quién es este Pokémon?</h1>
     <!-- <h3>{{ temerePokemon.nomen }}</h3> -->
-     <h3 class="capitalize my-3">{{ LudumStatus }}</h3>
+    <h3 class="capitalize my-3">{{ ludumStatus }}</h3>
 
-    <PokemonsImago :pokemon-id="temerePokemon.id"  :revelare_pokemon="LudumStatus !== LudumStatus.Ludit" />
-    <PokemonOptiones :optiones = "optiones" @electus-optio="examineResponsio"  />
+    <div class="h-20">
+      <button
+      v-if="LudumStatus === LudumStatus"
+      @click="sequentiOptiones(4)"
+      class="bg-blue-500 text-white p-2 rounded-xl hover:bg-blue-700 transition-all">
+        ¿Jugar de nuevo?
+      </button>
+    </div>
+
+    <PokemonsImago
+      :pokemon-id="temerePokemon.id"
+      :revelare_pokemon="ludumStatus !== LudumStatus.Ludit"
+    />
+    <PokemonOptiones
+      :optiones="optiones"
+      @electus-optio="examineResponsio"
+      :non-eligere="ludumStatus !== LudumStatus.Ludit"
+      :rected-responsio="temerePokemon.id"
+    />
   </section>
 </template>
 
@@ -18,17 +38,18 @@
 import PokemonOptiones from '../components/PokemonOptiones.vue';
 import PokemonsImago from '../components/PokemonsImago.vue';
 import { usePokemonLudum } from '../composables/usePokemonLudu';
+import { LudumStatus } from '../interfaces';
 
-const { LudumStatus,
+const {
+  ludumStatus,
   estPortat,
   temerePokemon,
-  pokemonOptiones:optiones,
-examineResponsio,
+  pokemonOptiones: optiones,
+  examineResponsio,
+  sequentiOptiones,
 } = usePokemonLudum();
 
-
 // const cumElectusOptio = (valorem:number) =>{}
-
 </script>
 
 <style scoped></style>
